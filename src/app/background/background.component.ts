50777a4fbef9549ac49b4aca61674467a4f18039
Arik Hativa <arikhativa@gmail.com>
2024-08-18 19:25:26 +0300

    better color random

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-background',
@@ -12,11 +12,30 @@ export class BackgroundComponent {
 
   lines: string[] = [];
   items: string[] = [];
+  background: string = 'rgb(255 255 255 / 25%)';
 
   ngOnInit() {
     const i = 30
     this.lines = Array(i);
     this.items = Array(i);
+  }
+
+  changeBackgroundColor(event: any) {
+    const randomColor = this.getRandomColor();
+    event.target.style.backgroundColor = randomColor;
+  }
+
+  resetBackgroundColor(event: any) {
+    event.target.style.backgroundColor = this.background;
+  }
+
+  getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
 }
