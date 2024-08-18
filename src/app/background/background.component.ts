7bd9da3cf2d56f1a331a9a92c23a7a1d83f55df5
Arik Hativa <arikhativa@gmail.com>
2024-08-18 14:19:43 +0300

    asd

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -8,13 +8,12 @@ import { Component, ElementRef, Renderer2, viewChild, ViewChild } from '@angular
   styleUrl: './background.component.scss',
 })
 export class BackgroundComponent {
+  @ViewChild('con') con!: ElementRef<HTMLDivElement>;
   @ViewChild('tile') tile!: ElementRef<HTMLDivElement>;
-  constructor(private hostElement: ElementRef<any>, private renderer: Renderer2) {
-  }
 
   ngAfterViewInit() {
-    const host = this.hostElement.nativeElement;
-    for (let i = 0; i < 10; i++) {
+    const host = this.con.nativeElement;
+    for (let i = 0; i < 500; i++) {
       host.appendChild(this.tile.nativeElement.cloneNode());
     }
   }
