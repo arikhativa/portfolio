fcb28235b716e4fe973d520d4e183fa3268018c8
Arik Hativa <arikhativa@gmail.com>
2024-08-18 15:41:47 +0300

    looks goog

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,20 +1,22 @@
-import { Component, ElementRef, Renderer2, viewChild, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-background',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss',
 })
 export class BackgroundComponent {
-  @ViewChild('con') con!: ElementRef<HTMLDivElement>;
-  @ViewChild('tile') tile!: ElementRef<HTMLDivElement>;
 
-  ngAfterViewInit() {
-    const host = this.con.nativeElement;
-    for (let i = 0; i < 500; i++) {
-      host.appendChild(this.tile.nativeElement.cloneNode());
-    }
+  lines: string[] = [];
+  items: string[] = [];
+
+  ngOnInit() {
+    const i = 35
+    this.lines = Array(i);
+    this.items = Array(i);
   }
+
 }
