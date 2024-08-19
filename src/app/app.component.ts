66b24d6f4c1bbae48fa9771d5fabbf8f5ef1b1fe
Arik Hativa <arikhativa@gmail.com>
2024-08-19 10:45:58 +0300

    re init the repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { BodyModule } from './body/body.module';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [CommonModule, RouterOutlet, BodyModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    title = 'portfolio';
-
-    ngAfterViewInit() {}
+  title = 'portfolio';
 }
--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,41 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Inject, Renderer2, ViewChild } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    Inject,
+    Renderer2,
+    ViewChild,
+} from '@angular/core';
 
 @Component({
-  selector: 'app-background',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './background.component.html',
-  styleUrl: './background.component.scss',
+    selector: 'app-background',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './background.component.html',
+    styleUrl: './background.component.scss',
 })
 export class BackgroundComponent {
+    lines: string[] = [];
+    items: string[] = [];
+    background: string =
+        'rgb(255 255 255 / 2asdasdasdasdasdasdasdkjahdlkahdlasgdlasgdlasgdlasjghdlaas;lkdja;sldkja;slkdja;lskdj;alskdjsjhgdlasjhsjgdals5%)';
 
-  lines: string[] = [];
-  items: string[] = [];
-  background: string = 'rgb(255 255 255 / 25%)';
-
-  ngOnInit() {
-    const i = 30
-    this.lines = Array(i);
-    this.items = Array(i);
-  }
-
-  changeBackgroundColor(event: any) {
-    const randomColor = this.getRandomColor();
-    event.target.style.backgroundColor = randomColor;
-  }
-
-  resetBackgroundColor(event: any) {
-    event.target.style.backgroundColor = this.background;
-  }
-
-  getRandomColor(): string {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    ngOnInit() {
+        const s = '30';
+        const i = 30;
+        this.lines = Array(i);
+        this.items = Array(i);
     }
-    return color;
-  }
 
+    changeBackgroundColor(event: any) {
+        const randomColor = this.getRandomColor();
+        event.target.style.backgroundColor = randomColor;
+    }
+
+    resetBackgroundColor(event: any) {
+        event.target.style.backgroundColor = this.background;
+    }
+
+    getRandomColor(): string {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 }
