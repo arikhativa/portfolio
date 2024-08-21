cf26476c4521a5c5812747523919fd4cf448745b
Arik Hativa <arikhativa@gmail.com>
2024-08-21 14:24:45 +0300

    init intro

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -1,17 +1,17 @@
-import { NavbarComponent } from './../navbar/navbar.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IntroComponent } from '../intro/intro.component';
-import { BackgroundComponent } from '../background/background.component';
-import { FormsModule } from '@angular/forms';
-import { DockModule } from 'primeng/dock';
-import { RadioButtonModule } from 'primeng/radiobutton';
+import { NavbarComponent } from "./../navbar/navbar.component"
+import { NgModule } from "@angular/core"
+import { CommonModule } from "@angular/common"
+import { IntroComponent } from "../intro/intro.component"
+import { BackgroundComponent } from "../background/background.component"
+import { FormsModule } from "@angular/forms"
+import { DockModule } from "primeng/dock"
+import { RadioButtonModule } from "primeng/radiobutton"
+import { YoavComponent } from "../yoav/yoav.component"
 
 @NgModule({
-    declarations: [NavbarComponent],
+    declarations: [NavbarComponent, IntroComponent, YoavComponent],
     imports: [
         CommonModule,
-        IntroComponent,
         BackgroundComponent,
         DockModule,
         RadioButtonModule,
---
+++ src/app/yoav/yoav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yoav',
+  templateUrl: './yoav.component.html',
+  styleUrl: './yoav.component.scss'
+})
+export class YoavComponent {
+
+}
