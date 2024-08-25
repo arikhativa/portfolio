1549f694d74ad169579314e72c2b89af77db2639
Arik Hativa <arikhativa@gmail.com>
2024-08-25 16:44:46 +0300

    added navbar

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -1,16 +1,21 @@
-import { NavbarComponent } from "./../navbar/navbar.component"
-import { NgModule } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { IntroComponent } from "../intro/intro.component"
-import { BackgroundComponent } from "../background/background.component"
-import { FormsModule } from "@angular/forms"
-import { DockModule } from "primeng/dock"
-import { RadioButtonModule } from "primeng/radiobutton"
-import { YoavComponent } from "../yoav/yoav.component";
+import { NavbarComponent } from './../navbar/navbar.component'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { IntroComponent } from '../intro/intro.component'
+import { BackgroundComponent } from '../background/background.component'
+import { FormsModule } from '@angular/forms'
+import { DockModule } from 'primeng/dock'
+import { RadioButtonModule } from 'primeng/radiobutton'
+import { YoavComponent } from '../yoav/yoav.component'
 import { RabbyComponent } from '../rabby/rabby.component'
 
 @NgModule({
-    declarations: [NavbarComponent, IntroComponent, YoavComponent, RabbyComponent],
+    declarations: [
+        NavbarComponent,
+        IntroComponent,
+        YoavComponent,
+        RabbyComponent,
+    ],
     imports: [
         CommonModule,
         BackgroundComponent,
