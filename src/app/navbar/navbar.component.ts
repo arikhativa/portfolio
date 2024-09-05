df2a0b7a41bea343ca5ec74b6c6bbbaac8d227de
Arik Hativa <arikhativa@gmail.com>
2024-09-05 11:58:05 +0300

    better navbar logic

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { MenuItem } from 'primeng/api'
+import { NavigationEnd, Router } from '@angular/router'
+import { filter } from 'rxjs'
 
 @Component({
     selector: 'app-navbar',
@@ -8,6 +9,24 @@ import { MenuItem } from 'primeng/api'
 })
 export class NavbarComponent {
     activeIndex: number = 0
+    routerLinks = ['/', '/about', '/projects', '/contact']
+
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        this.router.events
+            .pipe(
+                filter(
+                    (event): event is NavigationEnd =>
+                        event instanceof NavigationEnd
+                )
+            )
+            .subscribe(this.setIndexViaURL)
+    }
+
+    setIndexViaURL = (event: NavigationEnd) => {
+        this.activeIndex = this.routerLinks.indexOf(event.urlAfterRedirects)
+    }
 
     setActive(i: number) {
         this.activeIndex = i
