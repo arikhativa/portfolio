f200585e2deb307b4da250edaf4f15cc98f37093
Arik Hativa <arikhativa@gmail.com>
2024-09-23 14:56:35 +0200

    added postcss purge to npm build

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    css: ['./dist/portfolio/browser/*.css'],
+    content: [
+        './dist/portfolio/browser/index.html',
+        './dist/portfolio/browser/*.js',
+    ],
+    output: './dist/portfolio/browser/',
+}
