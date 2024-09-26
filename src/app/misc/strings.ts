f1f23ac497f50e410aa27d5e2d8fb2ba8a0c0828
Arik Hativa <arikhativa@gmail.com>
2024-09-26 10:37:07 +0200

    upcase and spelling

--- src/app/misc/strings.ts
+++ src/app/misc/strings.ts
@@ -3,12 +3,12 @@ import { Tab } from '../projects/projects.component'
 export const about = {
     cards: [
         {
-            header: 'hey',
-            text: 'i am yoav rabby.\nsome know me as arik hativa and others as pigafetta.\ni am 31. from a small village in israel.',
+            header: 'Hey',
+            text: 'I am Yoav Rabby.\nSome know me as Arik Hativa and others as Pigafetta.\nI am 31. From a small village in Israel.',
         },
         {
-            header: 'so... i am a software developer',
-            text: 'studied programming in infinity labs and in 42 madrid.\ni have 3 years of experience.',
+            header: 'so... I am a software developer',
+            text: 'Studied programming at Infinity Labs and at 42 Madrid.\nI have 3 years of professional experience.',
             // img: {
             //     dir: 'right',
             //     src: 'assets/images/42.png',
@@ -17,15 +17,15 @@ export const about = {
         },
         {
             header: 'Broadcom Inc.',
-            text: 'i work at broadcom for 2 years. mostly in a position of a full stack developer.\nafter a year there i got the respirability of the ELK stack and created a provisioning service in Golang.',
+            text: 'Work at Broadcom for 2 years. Mostly in a position of a full stack developer.\nAfter a year there i got the responsibility of the ELK stack and created a provisioning service in Golang.',
         },
         {
             header: 'Sofi Cooperative',
-            text: 'i joined sofi and started working on the mobile app "Avoda+" as a full stack developer.',
+            text: 'Joined Sofi and started working on the mobile app "Avoda+" as a full stack developer.',
         },
         {
-            header: 'web development',
-            text: 'i have also been making some websites as a freelancer.\ncheck them out!\n(they are under the briefcase icon)',
+            header: 'Web development',
+            text: 'I have also been making some websites as a freelancer.\nCheck them out!\n(they are under the briefcase icon)',
         },
     ],
 }
@@ -37,7 +37,7 @@ export const projects: {
         {
             title: 'Beit Tawfik Toubi',
             content:
-                '<a href="https://beittawfiktoubi.com">beittawfiktoubi.com</a><br><br>Beit Tawfik Toubi is a website for the small hosting business. written in Angular and hosted on Github Pages.',
+                '<a href="https://beittawfiktoubi.com">beittawfiktoubi.com</a><br><br>Beit Tawfik Toubi is a website for the small hosting business. Written in Angular and hosted on Github Pages.',
             tech: ['Angular', 'Typescript', 'Github Pages'],
         },
         {
@@ -58,28 +58,28 @@ export const projects: {
         {
             title: 'Naama Ofir',
             content:
-                '<a href="https://naamaofir.com/">naamaofir.com</a><br><br>Landing page for Naam Aofir (Animator). written in Angular and hosted on Github Pages.',
+                '<a href="https://naamaofir.com/">naamaofir.com</a><br><br>Landing page for Naama Ofir (Animator). Written in Angular and hosted on Github Pages.',
 
             tech: ['Angular', 'Typescript', 'Github Pages'],
         },
         {
             title: 'Oded Rabby',
             content:
-                '<a href="https://odedrabby.com/">odedrabby.com</a><br><br>Landing page for Oded Rabby. since he is a musician, I created a custom music player to the page. written in Angular and hosted on Github Pages.',
+                '<a href="https://odedrabby.com/">odedrabby.com</a><br><br>Landing page for Oded Rabby. Since he is a musician, I created a custom music player to the page. Written in Angular and hosted on Github Pages.',
 
             tech: ['Angular', 'Typescript', 'Github Pages'],
         },
         {
-            title: 'shemzmani',
+            title: 'Shemzmani',
             content:
-                "<a href=\"https://shemzmani.space/\">shemzmani.space</a><br><br>Landing page for Gal Cohen Paraira's new album 'shemzmani'. written in Angular and hosted on Github Pages.",
+                "<a href=\"https://shemzmani.space/\">shemzmani.space</a><br><br>Landing page for Gal Cohen Paraira's new album 'shemzmani'. Written in Angular and hosted on Github Pages.",
 
             tech: ['Angular', 'Typescript', 'Github Pages'],
         },
         {
             title: 'Milouban TLD',
             content:
-                '<a href="https://www.milouban.com/">milouban.com</a><br><br>Milouban LTD is a company that trades with cotton linter pulp. this website serves them as a landing page and to showcase their products. written with Wordpress, and hosted on Github Pages.',
+                '<a href="https://www.milouban.com/">milouban.com</a><br><br>Milouban LTD is a company that trades with cotton linter pulp. this website serves them as a landing page and to showcase their products. Written with Wordpress, and hosted on Github Pages.',
 
             tech: ['Wordpress', 'Docker', 'Github Pages'],
         },
