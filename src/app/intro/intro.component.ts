27a3a715a877c7e7402e06c746c311965f357dfd
Arik Hativa <arikhativa@gmail.com>
2024-10-02 13:42:50 +0200

    ChangeIntroFont

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core'
+import { getColorIndex, getNextFill } from '../misc/colorChange'
+
+export interface YoavRabbyColors {
+    y: string
+    o: string
+    a: string
+    v: string
+}
 
 @Component({
     selector: 'app-intro',
     templateUrl: './intro.component.html',
     styleUrl: './intro.component.scss',
 })
-export class IntroComponent {}
+export class IntroComponent {
+    lettersColors!: YoavRabbyColors
+
+    onMouseDown(event: MouseEvent): void {
+        event.preventDefault()
+        const t = event.target as HTMLElement
+        if (t && ['y', 'o', 'a', 'v'].includes(t.id)) {
+            const entry = t.id as keyof YoavRabbyColors
+
+            const index = getColorIndex(this.lettersColors[entry])
+            const fill = getNextFill(index)
+            // const stroke = getNextStroke(index)
+            this.lettersColors[entry] = fill
+            // this.lettersColors[entry].stroke = stroke
+        }
+    }
+}
